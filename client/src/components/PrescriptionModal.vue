9c6d86416f7ebde97e19e4a40972aa489b9a613f
Lewis England <lewis2004@hotmail.com>
2025-04-02 17:08:43 +0100

    feat(prescriptionModal): add basic form input validation for new prescriptions

--- client/src/components/PrescriptionModal.vue
+++ client/src/components/PrescriptionModal.vue
@@ -8,42 +8,45 @@
   >
     <b-container class="prescription-row">
       <b-row id="prescription-left-labels">
-        <b-col />
-        <b-col>
+        <b-col class="row-title"/>
+        <b-col class="form-header">
           SPH <b-icon-question-circle id="sph-tooltip" />
         </b-coL>
-        <b-col>
+        <b-col class="form-header">
           CYL <b-icon-question-circle id="cyl-tooltip" />
         </b-coL>
-        <b-col>
+        <b-col class="form-header">
           Axis
         </b-coL>
       </b-row>
       <b-row id="prescription-right-right">
-        <b-col>R:</b-col>
+        <b-col class="row-title">R</b-col>
         <b-col class="prescription-input">
-          <b-form-input id="right-sph-input" v-model="right.sph"/>
+          <b-form-input id="right-sph-input" v-model="right.sph" :formatter="format_number"/>
         </b-col>
         <b-col class="prescription-input">
-          <b-form-input id="right-cyl-input" v-model="right.cyl"/>
+          <b-form-input id="right-cyl-input" v-model="right.cyl" :formatter="format_number"/>
         </b-col>
         <b-col class="prescription-input">
-          <b-form-input id="right-axis-input" v-model="right.axis"/>
+          <b-form-input id="right-axis-input" v-model="right.axis" :formatter="format_number"/>
         </b-col>
       </b-row>
       <b-row id="prescription-left-eye">
-        <b-col>L:</b-col>
+        <b-col class="row-title">L</b-col>
         <b-col class="prescription-input">
-          <b-form-input id="left-sph-input" v-model="left.sph"/>
+          <b-form-input id="left-sph-input" v-model="left.sph" :formatter="format_number"/>
         </b-col>
         <b-col class="prescription-input">
-          <b-form-input id="left-cyl-input" v-model="left.cyl"/>
+          <b-form-input id="left-cyl-input" v-model="left.cyl" :formatter="format_number"/>
         </b-col>
         <b-col class="prescription-input">
-          <b-form-input id="left-axis-input" v-model="left.axis"/>
+          <b-form-input id="left-axis-input" v-model="left.axis" :formatter="format_number"/>
         </b-col>
       </b-row>
     </b-container>
+    <div id="validation-error" v-if="showValidationError">
+      Error: Please ensure all values are numbers in range
+    </div>
     <cyl-tooltip target="cyl-tooltip" />
     <sph-tooltip target="sph-tooltip" />
   </b-modal>
@@ -65,7 +68,13 @@ export default {
   data() {
     return {
       left: {axis: 0, cyl: 0, sph: 0},
-      right: {axis: 0, cyl: 0, sph: 0}
+      right: {axis: 0, cyl: 0, sph: 0},
+      showValidationError: false,
+      validation: {
+        min: -10,
+        max: 10,
+        decimal: 100
+      }
     }
   },
   methods: {
@@ -84,6 +93,16 @@ export default {
         this.resetForm()
       })
     },
+    format_number(e) {
+      if ((e > this.validation.max || e < this.validation.min) || Math.round(e * this.validation.decimal) / this.validation.decimal != e) {
+        this.showValidationError = true
+        return this.last_value
+      } else {
+        this.showValidationError = false
+        this.last_value = e
+        return e
+      }
+    },
     resetForm: function() {
       this.left = {axis: 0, cyl: 0, sph: 0}
       this.right = {axis: 0, cyl: 0, sph: 0}
@@ -91,3 +110,15 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.form-header {
+  padding-left: 25px;
+}
+
+.row-title {
+  text-align: right;
+  flex-grow: 0;
+  width: 30px;
+}
+</style>
